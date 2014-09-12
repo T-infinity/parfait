7d5f9d52c7e8a9aaab9ac151000d827fefe825a3
cdruyorj <cameron.t.druyorjr@nasa.gov>
2014-09-12 10:15:27 -0400

    merged parfait-vis.h into parfait.h

--- include/parfait.h
+++ include/parfait.h
@@ -33,7 +33,12 @@
 #include "unit_transformer.h"
 #include "vector_tools.h"
 #include "ugrid_writer.h"
-#include "parfait-vis.h"
+#include "vtk_extent_writer.h"
+#include "vtk_field.h"
+#include "vtk_generic_writer.h"
+#include "vtk_hex_writer.h"
+#include "vtk_slice_writer.h"
+#include "vtk_surface_writer.h"
 
 #ifdef PARFAIT_WITH_ANN
 #include "kd_tree.h"
