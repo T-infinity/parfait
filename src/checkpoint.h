e2388b68263da4b06fd94f48580c687230d13cd9
Matthew O'Connell <matthew.d.oconnell@nasa.gov>
2015-01-30 08:27:23 -0500

    To add edge map object and test.

---
+++ src/checkpoint.h
@@ -0,0 +1,8 @@
+#ifndef CHECKPOINT_H
+#define CHECKPOINT_H
+
+#include <stdio.h>
+#define CHECKPOINT fprintf(stderr, "CHECKPOINT: %s %d\n", __FILE__, __LINE__);
+
+
+#endif
---
+++ test/serial_tests/edge_map/edge_map_test.cpp
@@ -0,0 +1,17 @@
+#include "edge_map.h"
+#include "cart_mesh.h"
+
+#include <CppUTest/CommandLineTestRunner.h>
+
+TEST_GROUP(EdgeMap){};
+
+TEST(EdgeMap, CreateUniqueEdges){
+    CartMesh cartMesh({0,0,0}, {1,1,1}, 2, 2, 2);
+
+    EdgeMap<CartMesh> edgeMap(cartMesh);
+
+    LONGS_EQUAL(54, edgeMap.edges.size());
+
+    CHECK(edgeMap.getEdgeId({{1,2}}) == edgeMap.getEdgeId({{2,1}}));
+}
+
