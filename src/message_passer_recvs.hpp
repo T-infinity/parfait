63462faa785b4742ae8a566cd34d319060c7fada
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-03-18 09:15:44 -0400

    to put sends and recvs in correct files

--- src/message_passer_recvs.hpp
+++ src/message_passer_recvs.hpp
@@ -1,29 +1,32 @@
+// recv a vector (receiver knows length)
+template<typename T>
+void Recv(std::vector<T> &vec,int length,int source){
+    vec.clear();
+    vec.assign(length,0);
+    MPI_Status status;
+    MPI_Recv(&vec[0],length,Type(T()),source,0,MPI_COMM_WORLD,&status);
+}
 
-	// recv a vector (receiver knows length)
-	template<typename T>
-	void Recv(std::vector<T> &vec,int length,int source)
-	{
-		vec.clear();
-		vec.assign(length,0);
-		MPI_Status status;
-		MPI_Recv(&vec[0],length,Type(T()),source,0,MPI_COMM_WORLD,&status);
-	}
-	
-	// send a vector (receiver doesn't know length)
-	template<typename T>
-	void Send(std::vector<T> &vec,int destination)
-	{
-		int length = (int)vec.size();
-		Send(length,destination);
-		Send(vec,length,destination);
-	}
 
-	// recv a vector (receiver doesn't know length)
-	template<typename T>
-	void Recv(std::vector<T> &vec,int source)
-	{
-		vec.clear();
-		int length=0;
-		Recv(length,source);
-		Recv(vec,length,source);
-	}
+// recv a vector (receiver doesn't know length)
+template<typename T>
+void Recv(std::vector<T> &vec,int source){
+    vec.clear();
+    int length=0;
+    Recv(length,source);
+    Recv(vec,length,source);
+}
+
+// Recv a vector of vectors
+template<typename T>
+void Recv(std::vector<std::vector<T>> &vec,int source){
+    std::vector<int> recvBufferMap;
+    std::vector<T>   recvBuffer;
+    Recv(recvBufferMap,source);
+    Recv(recvBuffer,source);
+    vec.assign(recvBufferMap.size()-1,std::vector<T>());
+    // use map to unpack buffer into vector of vectors
+    for(int i=0;i<recvBufferMap.size()-1;i++)
+        for(int j=recvBufferMap[i];j<recvBufferMap[i+1];j++)
+            vec[i].push_back(recvBuffer[j]);
+}
--- src/message_passer_sends.hpp
+++ src/message_passer_sends.hpp
@@ -1,45 +1,33 @@
+// send a vector (receiver knows length)
+template<typename T>
+void Send(std::vector<T> &vec,int length,int destination){
+    T t;
+    MPI_Send(&vec[0],length,Type(t),destination,0,MPI_COMM_WORLD);
+}
 
-	
+// send a vector (receiver doesn't know length)
+template<typename T>
+void Send(std::vector<T> &vec,int destination){
+    int length = (int)vec.size();
+    Send(length,destination);
+    Send(vec,length,destination);
+}
 
+// send a vector of vectors
+template<typename T>
+void Send(std::vector<std::vector<T>> &vec,int destination){
+    // pack into a contiguous buffer
+    std::vector<T> sendBuffer;
+    std::vector<int> sendBufferMap = {0};
+    for(auto &row:vec)
+        sendBufferMap.push_back(row.size()+sendBufferMap.back());	
+    sendBuffer.reserve(sendBufferMap.back());
+    for(auto &row:vec)
+        for(auto val:row)
+            sendBuffer.push_back(val);
+    // send map to destination
+    Send(sendBufferMap,destination);
+    // send buffer to destination
+    Send(sendBuffer,destination);
+}
 
-	// send a vector (receiver knows length)
-	template<typename T>
-	void Send(std::vector<T> &vec,int length,int destination)
-	{
-		T t;
-		MPI_Send(&vec[0],length,Type(t),destination,0,MPI_COMM_WORLD);
-	}
-	
-	// send a vector of vectors
-	template<typename T>
-	void Send(std::vector<std::vector<T>> &vec,int destination)
-	{
-		// pack into a contiguous buffer
-		std::vector<T> sendBuffer;
-		std::vector<int> sendBufferMap = {0};
-		for(auto &row:vec)
-			sendBufferMap.push_back(row.size()+sendBufferMap.back());	
-		sendBuffer.reserve(sendBufferMap.back());
-		for(auto &row:vec)
-			for(auto val:row)
-				sendBuffer.push_back(val);
-		// send map to destination
-		Send(sendBufferMap,destination);
-		// send buffer to destination
-		Send(sendBuffer,destination);
-	}
-	
-	// Recv a vector of vectors
-	template<typename T>
-	void Recv(std::vector<std::vector<T>> &vec,int source)
-	{
-		std::vector<int> recvBufferMap;
-		std::vector<T>   recvBuffer;
-		Recv(recvBufferMap,source);
-		Recv(recvBuffer,source);
-		vec.assign(recvBufferMap.size()-1,std::vector<T>());
-		// use map to unpack buffer into vector of vectors
-		for(int i=0;i<recvBufferMap.size()-1;i++)
-			for(int j=recvBufferMap[i];j<recvBufferMap[i+1];j++)
-				vec[i].push_back(recvBuffer[j]);
-	}
