6537d7b206c26e2e5e620c27ac70d84e7b77a505
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-04-21 17:52:54 -0400

    can now add integer node data to binary vtu

--- src/vtk_volume_writer.cpp
+++ src/vtk_volume_writer.cpp
@@ -1,9 +1,18 @@
 #include "vtk_volume_writer.h"
 #include <vtkXMLUnstructuredGridWriter.h>
+#include <vtkIntArray.h>
+#include <vtkCellData.h>
+#include <vtkPointData.h>
 
 void VtkVolumeWriter::addNodeData(std::string name,
         int* data,int number_of_components){
-
+    vtkSmartPointer<vtkIntArray> stuff =
+        vtkSmartPointer<vtkIntArray>::New();
+    stuff->SetNumberOfComponents(1);
+    stuff->SetName(name.c_str());
+    for(int i=0;i<vtk_grid->GetNumberOfPoints();i++)
+        stuff->InsertNextValue(data[i]);
+    vtk_grid->GetPointData()->AddArray(stuff);
 }
 
 void VtkVolumeWriter::writeBinary(){
