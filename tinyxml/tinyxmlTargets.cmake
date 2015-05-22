ca013ec8ed5826eebdb0d2da62284f03b4684336
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-05-22 08:33:31 -0400

    deleted a bunch of garbage

--- tinyxml/tinyxmlTargets.cmake
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 2.8)
-
-project(tinyxml)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-
-include_directories("${TINYXML_CMAKE_DIR}/src")
-add_subdirectory("${TINYXML_CMAKE_DIR}/src" "${TINYXML_CMAKE_DIR}/build")
