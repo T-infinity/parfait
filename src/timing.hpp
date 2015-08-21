747237b118371cfa8feb4948745f963d420dffe5
matthewoconnell <matthew.d.oconnell@nasa.gov>
2015-08-21 09:51:58 -0400

    To relocate timing helper functions - and add the protective Parfait namespace

--- src/timing.hpp
+++ src/timing.hpp
@@ -1,4 +1,6 @@
-inline void printTimeDifference(Clock::time_point t0, Clock::time_point t1){
+namespace Parfait{
+  inline void printTimeDifference(Clock::time_point t0, Clock::time_point t1){
     milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
     std::cout << ms.count() << "ms\n";
+  }
 }
