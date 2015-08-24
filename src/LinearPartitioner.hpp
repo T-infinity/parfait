22527f4c75c52ed27c34298676423d3e4552fc5a
matthewoconnell <matthew.d.oconnell@nasa.gov>
2015-08-24 13:29:18 -0400

    To get localId from globalId in LinearPartitioner

--- src/LinearPartitioner.hpp
+++ src/LinearPartitioner.hpp
@@ -5,6 +5,12 @@
 
 namespace Parfait {
   namespace LinearPartitioner {
+
+    inline Range::Range(long start_in, long end_in)
+            : start(start_in), end(end_in) { }
+    inline OwnerLocalIdPair::OwnerLocalIdPair(long owner_in, long id_in)
+            : owner(owner_in), id(id_in) { }
+
     inline long computeStartGivenLeftOvers(long id, long work_per_worker, long left_overs) {
         long start;
         if (id <= left_overs)
@@ -37,6 +43,16 @@ namespace Parfait {
             return itemId / (work_items_per_worker+1);
         itemId -= num_work_items_left;
             return itemId / work_items_per_worker + num_left_workers;
+    }
+
+    inline OwnerLocalIdPair getOwnerLocalIdPairOfGlobalItem(long globalItemId, long numWorkItems, long numWorkers){
+        auto owner = getWorkerOfWorkItem(globalItemId, numWorkItems, numWorkers);
+        return {owner, getLocalIdOfItemOnWorker(globalItemId, owner, numWorkItems, numWorkers)};
+    }
+
+    inline long getLocalIdOfItemOnWorker(long globalItemId, long workerId, long numWorkItems, long numWorkers){
+        auto range = getRangeForWorker(workerId, numWorkItems, numWorkers);
+        return globalItemId - range.start;
     }
 
     inline Range getRangeForProc(long procId, long numWorkItems){
