a936fb6782104bae6835bc8865dfa9097bef2a7d
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-09-01 17:00:46 -0400

    Added cell winding converters for AflrToCGNS, CGNSToAflr, and CGNSToVtk.

---
+++ src/CellWindingConverters.h
@@ -0,0 +1,50 @@
+//
+// Created by cdruyorj on 9/1/15.
+//
+
+#ifndef PARFAIT_CELL_WINDING_CONVERTERS_H
+#define PARFAIT_CELL_WINDING_CONVERTERS_H
+
+#include <functional>
+
+namespace Parfait{
+    class CellWindingConverter{
+    public:
+        CellWindingConverter() = default;
+        void convertTet(int* tet) {};
+        void convertPyramid(int* pyramid) {}
+        void convertPrism(int* prism) {}
+        void convertHex(int* hex) {}
+    };
+
+    class AflrToCGNS : public CellWindingConverter{
+        public:
+            void convertPyramid(int *pyramid) {
+                int tmp = pyramid[2];
+                pyramid[2] = pyramid[3];
+                pyramid[3] = pyramid[4];
+                pyramid[4] = tmp;
+            }
+    };
+
+    class CGNSToAflr : public CellWindingConverter{
+    public:
+        void convertPyramid(int* pyramid){
+            int tmp = pyramid[2];
+            pyramid[2] = pyramid[4];
+            pyramid[4] = pyramid[3];
+            pyramid[3] = tmp;
+        }
+    };
+
+    class CGNSToVtk : public CellWindingConverter{
+    public:
+        void convertPrism(int* prism){
+            std::swap(prism[0],prism[3]);
+            std::swap(prism[1],prism[4]);
+            std::swap(prism[2],prism[5]);
+        }
+    };
+}
+
+#endif //PARFAIT_AFLRTOCGNS_H_H
