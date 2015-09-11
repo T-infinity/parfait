266394ba44af5d0370e3504225c51a94f1a598c2
matthewoconnell <matthew.d.oconnell@nasa.gov>
2015-09-11 15:30:22 -0400

    To migrate range loop

--- tests/serial_tests/range_loop/RangeLoopTests.cpp
+++ tests/serial_tests/range_loop/RangeLoopTests.cpp
@@ -1,43 +1,38 @@
-#include "RangeLoop.h"
+#include <catch.hpp>
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
-#include "CppUTest/CommandLineTestRunner.h"
+#include "RangeLoop.h"
 
-TEST_GROUP(RangeLoopTests)
-{
-};
-
-TEST(RangeLoopTests,Exists) {
+TEST_CASE("RangeLoopTests,Exists") {
     using namespace std;
     auto myRange = range(0, 2);
     auto begin = myRange.begin();
     auto end = myRange.end();
-    LONGS_EQUAL(0, *begin);
-    LONGS_EQUAL(2, *end);
+    REQUIRE(0 == *begin);
+    REQUIRE(2 == *end);
 
     ++begin;
-    LONGS_EQUAL(1, *begin);
+    REQUIRE(1 == *begin);
     ++begin;
-    CHECK_EQUAL(*begin, *end);
+    REQUIRE(*begin == *end);
 }
-TEST(RangeLoopTests, InsideARange)
-{
+TEST_CASE("RangeLoopTests, InsideARange") {
     std::vector<int> vec = {9, 10, 11, 12, 13, 14};
     int index = 0;
-    for (int i:range(9, 15)) LONGS_EQUAL(vec[index++], i);
+    for (int i:range(9, 15)) REQUIRE(vec[index++] == i);
 }
 
-TEST(RangeLoopTests, SingleNumber)
+TEST_CASE("RangeLoopTests, SingleNumber")
 {
     std::vector<int> vec = {0, 1, 2, 3, 4};
     int index = 0;
-    for (int i:range(5)) LONGS_EQUAL(vec[index++], i);
+    for (int i:range(5)) REQUIRE(vec[index++] == i);
 }
 
-TEST(RangeLoopTests,AutoVectorIndex){
+TEST_CASE("RangeLoopTests,AutoVectorIndex"){
 	using std::vector;
 	vector<int> vec(3,0);
     int index = 0;
-	for(int i:range(vec)) LONGS_EQUAL(index++, i);
+	for(int i:range(vec)) REQUIRE(index++ == i);
 }
