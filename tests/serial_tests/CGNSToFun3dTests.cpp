d013d14643b33c6a0930ce272b0f3bbc790628b5
matthewoconnell <matthew.d.oconnell@nasa.gov>
2015-09-11 14:35:55 -0400

    To migrate Fun3d

--- tests/serial_tests/CGNSToFun3dTests.cpp
+++ tests/serial_tests/CGNSToFun3dTests.cpp
@@ -1,59 +1,54 @@
 #include "CellWindingConverters.h"
-
-#include <CppUTest/CommandLineTestRunner.h>
+#include <catch.hpp>
 
 using namespace Parfait;
 
-
-TEST_GROUP(CGNSToFun3D){
-
-};
-TEST(CGNSToFun3D, tetsDontChange) {
+TEST_CASE("CGNSToFun3D, tetsDontChange") {
     std::array<int,4> tet {0,1,2,3};
     auto original = tet;
     auto converter = CGNSToFun3D();
     converter.convertTet(tet.data());
 
     for(int i=0;i<4;i++)
-        LONGS_EQUAL(original[i], tet[i]);
+        REQUIRE(original[i] == tet[i]);
 }
 
-TEST(CGNSToFun3D, pyramidsDontChange) {
+TEST_CASE("CGNSToFun3D, pyramidsDontChange") {
     std::array<int,5> pyramid {9,12,4,2,16};
     auto original = pyramid;
     auto converter = CGNSToFun3D();
     converter.convertPyramid(pyramid.data());
 
     for(int i=0;i<5;i++)
-        LONGS_EQUAL(pyramid[i], original[i]);
+        REQUIRE(pyramid[i] == original[i]);
 }
 
-TEST(CGNSToFun3D, prismsDoChange) {
+TEST_CASE("CGNSToFun3D, prismsDoChange") {
     std::array<int,6> prism {9,11,13,17,21,6};
     auto original = prism;
     auto converter = CGNSToFun3D();
     converter.convertPrism(prism.data());
 
-    LONGS_EQUAL(prism[0],9);
-    LONGS_EQUAL(prism[1],17);
-    LONGS_EQUAL(prism[2],21);
-    LONGS_EQUAL(prism[3],11);
-    LONGS_EQUAL(prism[4],6);
-    LONGS_EQUAL(prism[5],13);
+    REQUIRE(prism[0] == 9);
+    REQUIRE(prism[1] == 17);
+    REQUIRE(prism[2] == 21);
+    REQUIRE(prism[3] == 11);
+    REQUIRE(prism[4] == 6);
+    REQUIRE(prism[5] == 13);
 }
 
-TEST(CGNSToFun3D, hexsDoChange) {
+TEST_CASE("CGNSToFun3D, hexsDoChange") {
     std::array<int,8> hex {9,6,5,12,3,1,14,7};
     auto original = hex;
     auto converter = CGNSToFun3D();
     converter.convertHex(hex.data());
 
-    LONGS_EQUAL(hex[0],9);
-    LONGS_EQUAL(hex[1],3);
-    LONGS_EQUAL(hex[2],6);
-    LONGS_EQUAL(hex[3],1);
-    LONGS_EQUAL(hex[4],12);
-    LONGS_EQUAL(hex[5],7);
-    LONGS_EQUAL(hex[6],5);
-    LONGS_EQUAL(hex[7],14);
+    REQUIRE(hex[0] == 9);
+    REQUIRE(hex[1] == 3);
+    REQUIRE(hex[2] == 6);
+    REQUIRE(hex[3] == 1);
+    REQUIRE(hex[4] == 12);
+    REQUIRE(hex[5] == 7);
+    REQUIRE(hex[6] == 5);
+    REQUIRE(hex[7] == 14);
 }
