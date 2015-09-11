7175e8fada2505a96c624461b351e12b69624110
matthewoconnell <matthew.d.oconnell@nasa.gov>
2015-09-11 16:17:48 -0400

    To migrate vtk fields and exploded views

--- tests/serial_tests/VtkExplodedViewWriter/VtkExplodedViewWriterTests.cpp
+++ tests/serial_tests/VtkExplodedViewWriter/VtkExplodedViewWriterTests.cpp
@@ -1,20 +1,16 @@
+#include <catch.hpp>
 #include "CartMesh.h"
 #include "VtkExplodedViewWriter.h"
 #include "VtkGenericWriter.h"
 #include "ExplodedMesh.h"
-#include "CppUTest/CommandLineTestRunner.h"
 
-TEST_GROUP(VtkExplodedView){
-};
-
-TEST(VtkExplodedView, Exists){
+TEST_CASE("VtkExplodedView, Exists"){
     Parfait::CartMesh mesh({0,0,0},{1,1,1}, 10,10,10);
     Parfait::VtkExplodedViewWriter<Parfait::CartMesh> writer(mesh, "exploded");
     writer.writeAscii();
 }
 
-TEST_GROUP(ExplodedMesh){};
-TEST(ExplodedMesh, Exists){
+TEST_CASE("ExplodedMesh, Exists"){
     Parfait::CartMesh mesh({0,0,0},{1,1,1}, 2,2,2);
     Parfait::ExplodedMesh explodedMesh(mesh);
     CHECK(explodedMesh.numberOfCells() == mesh.numberOfCells());
--- tests/serial_tests/VtkFieldTests/VtkFieldTests.cpp
+++ tests/serial_tests/VtkFieldTests/VtkFieldTests.cpp
@@ -1,69 +1,57 @@
+#include <catch.hpp>
 #include "VtkField.h"
 #include <vector>
-#include "CppUTest/CommandLineTestRunner.h"
 
 using std::vector;
 using namespace Parfait;
 
-TEST_GROUP(VtkFieldTests)
-{
 
-};
-
-TEST(VtkFieldTests,TestInt)
-{
+TEST_CASE("VtkFieldTests,TestInt") {
 	vector<int> vec(4,1);
 	VtkField<int> field(true,"TestField",1,4,vec.data());
 	int x = field.getElement(0);
-	LONGS_EQUAL(1,x);
-	LONGS_EQUAL(INT32_FIELD,field.Type());
+	REQUIRE(1 == x);
+	REQUIRE(INT32_FIELD == field.Type());
 }
 
-TEST(VtkFieldTests,TestFloat)
-{
+TEST_CASE("VtkFieldTests,TestFloat") {
 	vector<float> vec(4,1.1);
 	VtkField<float> field(true,"TestField",1,4,vec.data());
 	float x = field.getElement(0);
-	DOUBLES_EQUAL(1.1,x,1.0e-5);
-	LONGS_EQUAL(FLOAT32_FIELD,field.Type());
+	REQUIRE(1.1 == Approx(x));
+	REQUIRE(FLOAT32_FIELD == field.Type());
 }
 
-TEST(VtkFieldTests,TestDouble)
-{
+TEST_CASE("VtkFieldTests,TestDouble") {
 	vector<double> vec(4,1.1);
 	VtkField<double> field(true,"TestField",1,4,vec.data());
 	double x = field.getElement(0);
-	DOUBLES_EQUAL(1.1,x,1.0e-15);
-	LONGS_EQUAL(FLOAT64_FIELD,field.Type());
+	REQUIRE(1.1 == Approx(x));
+	REQUIRE(FLOAT64_FIELD == field.Type());
 }
 
-TEST(VtkFieldTests,TestIntVectorData)
-{
+TEST_CASE("VtkFieldTests,TestIntVectorData") {
 	vector<int> vec(4,1);
 	VtkField<int> field(true,"TestField",4,1,vec.data());
 	const int *x = field.getBlock(0);
-	LONGS_EQUAL(1,x[0]);
+	REQUIRE(1 == x[0]);
 }
 
-TEST(VtkFieldTests,TestFloatVectorData)
-{
+TEST_CASE("VtkFieldTests,TestFloatVectorData") {
 	vector<float> vec(4,1.1);
 	VtkField<float> field(true,"TestField",4,1,vec.data());
 	const float *x = field.getBlock(0);
-	DOUBLES_EQUAL(1.1,x[0],1.0e-5);
+	REQUIRE(1.1 == Approx(x[0]));
 }
 
-TEST(VtkFieldTests,TestDoubleVectorData)
-{
+TEST_CASE("VtkFieldTests,TestDoubleVectorData") {
 	vector<double> vec(4,1.1);
 	VtkField<double> field(true,"TestField",4,1,vec.data());
 	const double *x = field.getBlock(0);
-	DOUBLES_EQUAL(1.1,x[0],1.0e-15);
+	REQUIRE(1.1 == x[0]);
 }
 
-TEST(VtkFieldTests,TestAppend)
-{
+TEST_CASE("VtkFieldTests,TestAppend") {
 	vector<double> vec(1,1.1);
 	VtkField<double> field(false,"TestField",1,1,vec.data());
-	//field.append("appended_field.dat");
 }
