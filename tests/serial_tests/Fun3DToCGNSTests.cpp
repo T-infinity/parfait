e9ddb0db6d012efe0c67554e4db56d7ff9b7516a
matthewoconnell <matthew.d.oconnell@nasa.gov>
2015-09-11 14:54:19 -0400

    To migrade fun3d to cgns

--- tests/serial_tests/Fun3DToCGNSTests.cpp
+++ tests/serial_tests/Fun3DToCGNSTests.cpp
@@ -1,59 +1,56 @@
+#include <catch.hpp>
 #include "CellWindingConverters.h"
 
-#include <CppUTest/CommandLineTestRunner.h>
 
 using namespace Parfait;
 
-TEST_GROUP(Fun3DToCGNS){
-
-};
-TEST(Fun3DToCGNS, tetsDontChange) {
+TEST_CASE("Fun3DToCGNS, tetsDontChange") {
     std::array<int,4> tet {0,1,2,3};
     auto original = tet;
     auto converter = Fun3DToCGNS();
     converter.convertTet(tet.data());
 
     for(int i=0;i<4;i++)
-    LONGS_EQUAL(original[i], tet[i]);
+    REQUIRE(original[i] == tet[i]);
 }
 
-TEST(Fun3DToCGNS, pyramidsDontChange) {
+TEST_CASE("Fun3DToCGNS, pyramidsDontChange") {
     std::array<int,5> pyramid {9,12,4,2,16};
     auto original = pyramid;
     auto converter = Fun3DToCGNS();
     converter.convertPyramid(pyramid.data());
 
     for(int i=0;i<5;i++)
-    LONGS_EQUAL(pyramid[i], original[i]);
+    REQUIRE(pyramid[i] == original[i]);
 }
 
-TEST(Fun3DToCGNS, prismsDoChange) {
+TEST_CASE("Fun3DToCGNS, prismsDoChange") {
     std::array<int,6> prism {9,17,21,11,6,13};
     auto original = prism;
     auto converter = Fun3DToCGNS();
     converter.convertPrism(prism.data());
 
-    LONGS_EQUAL(prism[0],9);
-    LONGS_EQUAL(prism[1],11);
-    LONGS_EQUAL(prism[2],13);
-    LONGS_EQUAL(prism[3],17);
-    LONGS_EQUAL(prism[4],21);
-    LONGS_EQUAL(prism[5],6);
+    REQUIRE(prism[0] == 9);
+    REQUIRE(prism[1] == 11);
+    REQUIRE(prism[2] == 13);
+    REQUIRE(prism[3] == 17);
+    REQUIRE(prism[4] == 21);
+    REQUIRE(prism[5] == 6);
 }
 
-TEST(Fun3DToCGNS, hexsDoChange) {
+TEST_CASE("Fun3DToCGNS, hexsDoChange") {
     std::array<int,8> hex {9,3,6,1,12,7,5,14};
     auto original = hex;
     auto converter = Fun3DToCGNS();
     converter.convertHex(hex.data());
 
-    LONGS_EQUAL(hex[0],9);
-    LONGS_EQUAL(hex[1],6);
-    LONGS_EQUAL(hex[2],5);
-    LONGS_EQUAL(hex[3],12);
-    LONGS_EQUAL(hex[4],3);
-    LONGS_EQUAL(hex[5],1);
-    LONGS_EQUAL(hex[6],14);
-    LONGS_EQUAL(hex[7],7);
+    REQUIRE(hex[0] == 9);
+    REQUIRE(hex[1] == 6);
+    REQUIRE(hex[2] == 5);
+    REQUIRE(hex[3] == 12);
+    REQUIRE(hex[4] == 3);
+    REQUIRE(hex[5] == 1);
+    REQUIRE(hex[6] == 14);
+    REQUIRE(hex[7] == 7);
 }
 
--- tests/serial_tests/VtkToCGNSTests.cpp
+++ tests/serial_tests/VtkToCGNSTests.cpp
@@ -1,53 +1,46 @@
+#include <catch.hpp>
 #include "CellWindingConverters.h"
-
-#include <CppUTest/CommandLineTestRunner.h>
-
 using namespace Parfait;
 
-
-TEST_GROUP(VtkToCGNS){
-
-};
-
-TEST(VtkToCGNS, tetsDontChange) {
+TEST_CASE("VtkToCGNS, tetsDontChang") {
     std::array<int,4> tet {0,1,2,3};
     auto original = tet;
     auto converter = VtkToCGNS();
     converter.convertTet(tet.data());
 
     for(int i=0;i<4;i++)
-        LONGS_EQUAL(original[i], tet[i]);
+        REQUIRE(original[i] == tet[i]);
 }
-TEST(VtkToCGNS, pyramidsDontChange) {
+TEST_CASE("VtkToCGNS, pyramidsDontChange") {
     std::array<int,5> pyramid {9,12,4,2,16};
     auto original = pyramid;
     auto converter = VtkToCGNS();
     converter.convertPyramid(pyramid.data());
 
     for(int i=0;i<5;i++)
-        LONGS_EQUAL(pyramid[i],original[i]);
+        REQUIRE(pyramid[i] == original[i]);
 }
 
-TEST(VtkToCGNS, prismsDontChange) {
+TEST_CASE("VtkToCGNS, prismsDontChange") {
     std::array<int,6> prism {0,1,2,3,4,5};
     auto original = prism;
     auto converter = VtkToCGNS();
     converter.convertPrism(prism.data());
 
-    LONGS_EQUAL(original[0], prism[3]);
-    LONGS_EQUAL(original[1], prism[4]);
-    LONGS_EQUAL(original[2], prism[5]);
-    LONGS_EQUAL(original[3], prism[0]);
-    LONGS_EQUAL(original[4], prism[1]);
-    LONGS_EQUAL(original[5], prism[2]);
+    REQUIRE(original[0] == prism[3]);
+    REQUIRE(original[1] == prism[4]);
+    REQUIRE(original[2] == prism[5]);
+    REQUIRE(original[3] == prism[0]);
+    REQUIRE(original[4] == prism[1]);
+    REQUIRE(original[5] == prism[2]);
 }
 
-TEST(VtkToCGNS, hexsDontChange) {
+TEST_CASE("VtkToCGNS, hexsDontChange") {
     std::array<int,8> hex {0,1,2,3,4,5,6,7};
     auto original = hex;
     auto converter = VtkToCGNS();
     converter.convertHex(hex.data());
 
     for(int i=0;i<8;i++)
-        LONGS_EQUAL(original[i], hex[i]);
+        REQUIRE(original[i] == hex[i]);
 }
