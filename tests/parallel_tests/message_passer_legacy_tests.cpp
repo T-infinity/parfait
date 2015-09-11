7e1af6284374dc74c6be5aed3520c2bd23a9844b
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-09-11 14:40:07 -0400

    Extracted unit tests for

--- tests/parallel_tests/message_passer_legacy_tests.cpp
+++ tests/parallel_tests/message_passer_legacy_tests.cpp
@@ -39,63 +39,6 @@ TEST(MessagePasserTests,Exists)
 	
 	Barrier();
 
-	if(Rank() == 0) {
-		int value = 0,sum=0,check_sum=0;
-		for(int i=1;i<NumberOfProcesses();i++) {
-			Recv(value,i);
-			sum += value;
-			check_sum += i;
-		}
-		LONGS_EQUAL(check_sum,sum);
-	}
-	else {
-		Send(Rank(),0);
-	}
-
-	// test Send/Recv for floats
-	if(Rank() == 0) {
-		float value = 0,sum=0,check_sum=0;
-		for(int i=1;i<NumberOfProcesses();i++)
-		{
-			Recv(value,i);
-			sum += value;
-			check_sum += i;
-		}
-		LONGS_EQUAL(check_sum,sum);
-	}
-	else {
-		Send((float)Rank(),0);
-	}
-
-	// test Send/Recv for doubles
-	if(Rank() == 0) {
-		double value = 0,sum=0,check_sum=0;
-		for(int i=1;i<NumberOfProcesses();i++)
-		{
-			Recv(value,i);
-			sum += value;
-			check_sum += i;
-		}
-		LONGS_EQUAL(check_sum,sum);
-	}
-	else {
-		Send((double)Rank(),0);
-	}
-
-	// test Recv from any source for integers
-	if(Rank() == 0) {
-		int value = 0,sum=0,check_sum=0;
-		for(int i=1;i<NumberOfProcesses();i++)
-		{
-			Recv(value);
-			sum += value;
-			check_sum += i;
-		}
-		LONGS_EQUAL(check_sum,sum);
-	}
-	else {
-		Send(Rank(),0);
-	}
 
 	// test Send for vector of integers
 	{
