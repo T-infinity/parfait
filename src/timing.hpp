590eb2510ebcdbc2010885f805a2d72fd843226e
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-09-16 10:06:42 -0400

    updated printReadableElapsedTime to automatically print out resolution down to milliseconds.

--- src/timing.hpp
+++ src/timing.hpp
@@ -13,17 +13,19 @@ namespace Parfait{
         hours h = std::chrono::duration_cast<hours>(t1-t0);
         minutes m = std::chrono::duration_cast<minutes>(t1-t0);
         seconds s = std::chrono::duration_cast<seconds>(t1-t0);
+        milliseconds ms = std::chrono::duration_cast<milliseconds>(t1-t0);
 
         long number_of_hours = h.count();
         long number_of_minutes = m.count() - 60*number_of_hours;
         long number_of_seconds = s.count() - 360*number_of_hours - 60*number_of_minutes;
-
+        long number_of_milliseconds = ms.count() - 3600000*number_of_hours
+                                                 - 60000*number_of_minutes
+                                                 - 1000*number_of_seconds;
         if(0 < number_of_hours)
             std::cout << number_of_hours << " hours ";
         if(0 < number_of_minutes)
             std::cout << number_of_minutes << " minutes ";
-        if(0 < number_of_seconds)
-            std::cout << number_of_seconds << " seconds";
+        std::cout << number_of_seconds << "." << number_of_milliseconds << " seconds";
         std::cout << std::endl;
     }
 }
