4870c530fd26c47815bd5aa7175c3cd48960c803
Matthew O'Connell <Matthew.D.OConnell@nasa.gov>
2015-10-22 12:45:31 -0400

    To create unit tests for determining needed and owned global node ids.

--- tests/parallel_tests/PreProcessor/SendRecvPairTests.cpp
+++ tests/parallel_tests/PreProcessor/SendRecvPairTests.cpp
@@ -4,9 +4,8 @@
 #include <NodeCenteredRedistributor.h>
 #include <SendReceivePairCreator.h>
 
-TEST_CASE("Send Recv Pairs Exist"){
-    if(MessagePasser::NumberOfProcesses() != 2) return;
 
+std::shared_ptr<Parfait::ParallelMesh> getTestMesh(){
     auto mesh = std::make_shared<Parfait::ParallelMesh>();
     if(MessagePasser::Rank() == 0){
         mesh->connectivity->prisms = {0,1,2,3,4,5};
@@ -26,12 +25,30 @@ TEST_CASE("Send Recv Pairs Exist"){
         mesh->metaData->nodeComponentIds = {0,0,0};
         mesh->metaData->xyz = {0,0,1, 1,0,1, 1,1,1};
     }
+    return mesh;
+}
+
+TEST_CASE("Send Recv Pairs") {
+    if(MessagePasser::NumberOfProcesses() != 2) return;
+    std::shared_ptr<Parfait::ParallelMesh> mesh = getTestMesh();
     std::vector<int> part(3, MessagePasser::Rank());
     Parfait::NodeBasedRedistributor reDistributor(mesh, part);
     mesh = reDistributor.redistribute();
 
-    auto pair = Parfait::SendReceivePairCreator::createNodeBasedPair(mesh);
-    REQUIRE(pair.send.size() == 3);
-    int other_rank = MessagePasser::Rank() == 0 ? 1 : 0;
-    REQUIRE(pair.send[0].size() == 1);
+    Parfait::SendReceivePairCreator::NodePairCreator creator(mesh);
+    auto needed = creator.findNeededNodes();
+    if(MessagePasser::Rank() == 0){
+        REQUIRE((needed == std::vector<long>{3,4,5}));
+    }
+    if(MessagePasser::Rank() == 1){
+        REQUIRE((needed == std::vector<long>{0,1,2}));
+    }
+
+    auto owned = creator.getOwnedNodeGlobalIds();
+    if(MessagePasser::Rank() == 0){
+        REQUIRE((owned == std::vector<long>{0,1,2}));
+    }
+    if(MessagePasser::Rank() == 1){
+        REQUIRE((owned == std::vector<long>{3,4,5}));
+    }
 }
