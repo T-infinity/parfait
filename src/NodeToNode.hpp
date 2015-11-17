f5243c4f0df76c33c5ce04b6326560f1c99e5d96
Cameron T. Druyor <cameron.t.druyorjr@nasa.gov>
2015-11-17 13:28:23 -0500

    digging into tet edge processing

--- src/NodeToNode.hpp
+++ src/NodeToNode.hpp
@@ -7,6 +7,7 @@ std::vector <std::vector<int>> Parfait::NodeToNodeBuilder<MeshType>::buildNodeTo
     if(MessagePasser::Rank() == 0) {printf("--tet edges\n");fflush(stdout);}
     for(int cellId = 0; cellId < mesh.numberOfTets(); cellId++){
         auto cell = mesh.getTet(cellId);
+        if(MessagePasser::Rank() == 0 and cellId %1000 == 0) {printf("--tet %i of %i\n",cellId,mesh.numberOfTets());fflush(stdout);}
         for(int edge = 0; edge < CGNS::Edges::Tet::numberOfEdges(); edge++){
             auto e = CGNS::Edges::Tet::getEdge(cell, edge);
             addEdge(e[0], e[1]);
