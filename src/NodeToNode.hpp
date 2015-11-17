24633ef533e5ef0d3a10cae9dc32aa4069d743a0
Cameron T. Druyor <cameron.t.druyorjr@nasa.gov>
2015-11-17 15:14:21 -0500

    to print each tet

--- src/NodeToNode.hpp
+++ src/NodeToNode.hpp
@@ -6,9 +6,10 @@ template<typename MeshType>
 std::vector <std::vector<int>> Parfait::NodeToNodeBuilder<MeshType>::   buildNodeToNodeConnectivity() {
     if(MessagePasser::Rank() == 0) {printf("--tet edges\n");fflush(stdout);}
     for(int cellId = 0; cellId < mesh.numberOfTets(); cellId++){
+        if(MessagePasser::Rank() == 0) {printf("--tet %i of %i\n",cellId,mesh.numberOfTets());fflush(stdout);}
         const int* ptr = mesh.getTet(cellId);
+        if(MessagePasser::Rank() == 0) {printf("--(%i %i %i %i)\n",ptr[0],ptr[1],ptr[2],ptr[3]);fflush(stdout);}
         std::array<int,4> cell {ptr[0],ptr[1],ptr[2],ptr[3]};
-        if(MessagePasser::Rank() == 0 and cellId %1000 == 0) {printf("--tet %i of %i\n",cellId,mesh.numberOfTets());fflush(stdout);}
         for(int edge = 0; edge < CGNS::Edges::Tet::numberOfEdges(); edge++){
             auto e = CGNS::Edges::Tet::getEdge(cell, edge);
             addEdge(e[0], e[1]);
