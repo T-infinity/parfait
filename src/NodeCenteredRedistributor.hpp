3456f12d1d53fea112e16224483531b30cc06c93
cdruyorj <cameron.t.druyorjr@nasa.gov>
2015-11-20 16:06:29 -0500

    switching back to find bug

--- src/NodeCenteredRedistributor.hpp
+++ src/NodeCenteredRedistributor.hpp
@@ -250,17 +250,17 @@ namespace Parfait {
             if (MessagePasser::Rank() == proc)
                 neededNodeIds = my_non_ghost_ids;
             MessagePasser::Broadcast(neededNodeIds, proc);
-            for(auto id:neededNodeIds){
-                auto iter = oldGlobalToLocal.find(id);
-                if(iter != oldGlobalToLocal.end()){
-                    for(int cellId:nodeToCell[iter->second])
-                        sendCellIds.push_back(cellId);
-                }
-            }
-            //for (unsigned int i = 0; i < cells.size() / cellSize; i++) {
-            //    if(iShouldSendThisCell(&cells[cellSize*i],cellSize,neededNodeIds))
-            //        sendCellIds.push_back(i);
+            //for(auto id:neededNodeIds){
+            //    auto iter = oldGlobalToLocal.find(id);
+            //    if(iter != oldGlobalToLocal.end()){
+            //        for(int cellId:nodeToCell[iter->second])
+            //            sendCellIds.push_back(cellId);
+            //    }
             //}
+            for (unsigned int i = 0; i < cells.size() / cellSize; i++) {
+                if(iShouldSendThisCell(&cells[cellSize*i],cellSize,neededNodeIds))
+                    sendCellIds.push_back(i);
+            }
             vector<long> sendCells;
             sendCells.reserve(sendCellIds.size());
             for (auto id:sendCellIds)
