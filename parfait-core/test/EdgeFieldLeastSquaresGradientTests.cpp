6ee1436f9b3b227b631792ced4b2dee82455e231
Matthew O'Connell <Matthew.D.OConnell@nasa.gov>
2017-07-25 11:00:12 -0400

    To compute hessian and gradients from mesh interface.

--- parfait-core/test/EdgeFieldLeastSquaresGradientTests.cpp
+++ parfait-core/test/EdgeFieldLeastSquaresGradientTests.cpp
@@ -14,14 +14,14 @@ TEST_CASE("Edge collection least squares gradient of more than 1 variable"){
         var[i][1] = 5.5*p[0] + 1.1*p[1] + 3.3*p[2];
     }
 
-    auto gs = calcGSWeights(getEdge, edges.size(), getXyz, points.size());
+    auto gs = Parfait::Gradient::calcGSWeights(getEdge, edges.size(), getXyz, points.size());
 
     auto getFirstVariable = [&](int node_id){return var[node_id][0];};
     auto getSecondVariable = [&](int node_id){return var[node_id][1];};
 
-    auto grad_first = calculateNodeGradientsFromEdges(getEdge, edges.size(), getXyz, points.size(),
+    auto grad_first = Parfait::Gradient::calculateNodeGradientsFromEdges(getEdge, edges.size(), getXyz, points.size(),
                                                       gs, getFirstVariable);
-    auto grad_second = calculateNodeGradientsFromEdges(getEdge, edges.size(), getXyz, points.size(),
+    auto grad_second = Parfait::Gradient::calculateNodeGradientsFromEdges(getEdge, edges.size(), getXyz, points.size(),
                                                        gs, getSecondVariable);
 
     REQUIRE(grad_first.size() == points.size());
