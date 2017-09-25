6a783bfd2084435332ec7800858e3f6786dd0a01
Matthew O'Connell <Matthew.D.OConnell@nasa.gov>
2017-09-25 12:32:14 -0400

    To demonstrate how to do edge based gradient

--- parfait/LeastSquaresGradient.h
+++ parfait/LeastSquaresGradient.h
@@ -1,10 +1,11 @@
+#pragma once
 #include <vector>
 
 namespace Parfait {
 
     struct GSWeights {
     public:
-        GSWeights(double a, double b, double c, double d, double e, double f)
+        inline GSWeights(double a, double b, double c, double d, double e, double f)
                 : r11(a), r12(b), r13(c), r22(d), r23(e), r33(f) {}
 
         double r11, r12, r13, r22, r23, r33;
