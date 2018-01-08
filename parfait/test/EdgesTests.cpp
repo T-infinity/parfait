8f1dc28e8dc6c68883150804cc9bd91d571dc12b
Matthew O'Connell <Matthew.D.OConnell@nasa.gov>
2018-01-08 13:32:10 -0500

    To remove redundant tests

--- parfait/test/EdgesTests.cpp
+++ parfait/test/EdgesTests.cpp
@@ -40,15 +40,3 @@ TEST_CASE("add cells to edge list"){
     REQUIRE(5 == edges.front()[0]);
     REQUIRE(9 == edges.front()[1]);
 }
-
-TEST_CASE("add cells to edge list"){
-    Parfait::Edges::EdgeBuilder edgeBuilder;
-    std::array<int,4> tet {9,5,30,32};
-    edgeBuilder.addCell(tet.data(),Parfait::CGNS::Tet::edge_to_node);
-
-    auto edges = edgeBuilder.edges();
-
-    REQUIRE(6 == edges.size());
-    REQUIRE(5 == edges.front()[0]);
-    REQUIRE(9 == edges.front()[1]);
-}
